c873ad3cc92ab496165629841f0d37ca3a1e5eb6
poonam678 <poonam.patil.a@gmil.com>
2026-06-22 16:45:57 +0530

    Pushing Login code

---
+++ tests/example.spec.js
@@ -0,0 +1,14 @@
+
+import {test} from "@playwright/test"
+
+test("css selectors",async({page})=>{
+    await page.goto("https://www.saucedemo.com")
+    //select by ID
+    await page.locator('#name').fill("amith")
+    //select by tag+id
+    await page.locator("input#email").fill("amith123@getMaxListeners.com")
+    //select by attribute 
+    await page.locator("[type='password]").fill("amith12345")
+    //select by tag+attribute
+    await page.locator('button[type="submit"]').click()
+})
